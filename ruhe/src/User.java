c7428c32666fe594b22e33a71ea26c34befac42b
linhui <lin970308520@163.com>
2023-06-30 21:34:01 +0800

    first

---
+++ ruhe/src/User.java
@@ -0,0 +1,13 @@
+/**
+ * ruhe PACKAGE_NAME
+ * 2023-06-30 20:03
+ *
+ * @author linhui 86188
+ * description : 不可描述
+ * @data 2023/6/30 20:03
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ test/src/com/uniftech/Name.java
@@ -0,0 +1,12 @@
+package com.uniftech;
+
+/**
+ * ruhe com.uniftech
+ * 2023-06-30 21:27
+ *
+ * @author linhui 86188
+ * description : 不可描述
+ * @data 2023/6/30 21:27
+ */
+public class Name {
+}
